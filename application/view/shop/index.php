7d9cc32dc3d6c78edfd08ff02304b593018877b3
Tristan-BS <tristanbirni@gmail.com>
2026-06-25 08:28:13 +0200

    IMPLEMENTED | easy shop / cart switch - REMOVED | unnecessary double code for showing products

--- application/view/shop/index.php
+++ application/view/shop/index.php
@@ -33,7 +33,6 @@
                     <br><br><br>
                     <label>Select multiple product images</label>
                     <input type="file" name="fileUpload[]" id="fileUpload" multiple="multiple" accept=".png, .jpg, .jpeg, .webpb, .svg">
-                    <!-- <input type="submit" value="Upload Image" name="submit"> -->
                     <br>
 
                     <br><br>
@@ -58,96 +57,75 @@
 
             <?php endif; ?>
 
-            <div class="shopping-cart">
-                <button onclick="switchVisibility('product-list', 'shopping-cart')">view shopping cart</button>
+            <div class="view-toggle">
+                <button id="btn-toggle-view" onclick="toggleView()">View Shopping Cart</button>
             </div>
 
-            <div id="shopping-cart">
-                <?php 
-                    if (!empty($this->shoppingCart)) {
-                        foreach ($this->shoppingCart as $product) { ?>
-                            <div class="product">
-                                <?php
-                                    $hasImage = false;
-                                    foreach ($this->productImages as $image) {
-                                        if ($image->product_id != $product->id) continue;
-                                        
-                                        $hasImage = true;
-                                        $imagePath = Config::get('URL') . 'shopImages/' . $image->product_id . '/' . $image->name;
-                                    ?>
-                                        <div class="image-container">
-                                            <img class="product-image" src="<?php echo $imagePath; ?>">
-                                        </div>
-                                    <?php }
-                                    if (!$hasImage) { ?>
-                                        <div class="image-container">
-                                            <img class="product-image" src="<?php echo Config::get('URL'); ?>shopImages/placeholder/placeholder.png">
-                                        </div>
-                                <?php } ?>
-                                
-                                <div class="detail-container">
-                                    <p class="product-name"><?php echo $product->name; ?></p>
-                                    <p class="product-description"><?php echo $product->description; ?></p>
-                                    <p class="product-category"><?php echo $product->category_name; ?></p>
-                                    
-                                    <form action="<?php echo Config::get('URL'); ?>shop/removeFromCart" method="post">
-                                        <input type="hidden" name="productID" value="<?php echo $product->id; ?>">
-                                        <button type="submit" class="button">Remove Item from cart</button>
-                                    </form>
+            <?php
+                $cartProductIds = [];
+                foreach ($this->shoppingCart as $cartItem) {
+                    $cartProductIds[$cartItem->product_id] = true;
+                }
+            ?>
 
-                                </div>
-                            </div>
-                        <?php }
-                    }
-                else { ?>
-                    <p>No products in your shopping cart.</p>
-                <?php }?>
-            </div>
-
-            <br><br>
-            <div class="product-container" id="product-list">
-                <?php foreach ($this->products as $product) { ?>
-
-                    <div class="product">
+            <div id="view-container" class="mode-shop">
+                <?php foreach ($this->products as $product) {
+                    $inCart = isset($cartProductIds[$product->id]);
+                ?>
+                    <div class="product" data-in-cart="<?= $inCart ? 'true' : 'false' ?>">
                         <?php
                             $hasImage = false;
                             foreach ($this->productImages as $image) {
                                 if ($image->product_id != $product->id) continue;
-                                
                                 $hasImage = true;
                                 $imagePath = Config::get('URL') . 'shopImages/' . $image->product_id . '/' . $image->name;
-                            ?>
+                                ?>
                                 <div class="image-container">
                                     <img class="product-image" src="<?php echo $imagePath; ?>">
                                 </div>
-                            <?php }
+                                <?php
+                                break;
+                            }
                             if (!$hasImage) { ?>
                                 <div class="image-container">
                                     <img class="product-image" src="<?php echo Config::get('URL'); ?>shopImages/placeholder/placeholder.png">
                                 </div>
                         <?php } ?>
-                        
+
                         <div class="detail-container">
                             <p class="product-name"><?php echo $product->name; ?></p>
                             <p class="product-description"><?php echo $product->description; ?></p>
                             <p class="product-category"><?php echo $product->category_name; ?></p>
-                            
-                            <?php if(!empty($product->inventory_amount)) { ?>
-                                <p>Inventory amount: <?php echo $product->inventory_amount; ?></p>
 
-                                <form action="<?php echo Config::get('URL'); ?>shop/addToCart" method="post">
+                            <div class="shop-only">
+                                <?php if (!empty($product->inventory_amount)) { ?>
+                                    <p>Inventory: <?php echo $product->inventory_amount; ?></p>
+                                    <form action="<?php echo Config::get('URL'); ?>shop/addToCart" method="post">
+                                        <input type="hidden" name="productID" value="<?php echo $product->id; ?>">
+                                        <input type="number" class="product-amount-input" name="productAmount" min="1" max="<?php echo $product->inventory_amount; ?>" value="1">
+                                        <button type="submit" class="button">Place into cart</button>
+                                    </form>
+                                <?php } else { ?>
+                                    <p class="no-inventory-text">This product is currently unavailable</p>
+                                <?php } ?>
+                            </div>
+
+                            <div class="cart-only">
+                                <form action="<?php echo Config::get('URL'); ?>shop/removeFromCart" method="post">
                                     <input type="hidden" name="productID" value="<?php echo $product->id; ?>">
-
-                                    <input type="number" id="productAmountInput" name="productAmount" min="1" max="<?php echo $product->inventory_amount; ?>" value="1">
-                                    <button type="submit" class="button">Place into cart</button>
+                                    <button type="submit" class="button">Remove from cart</button>
                                 </form>
-
-                            <?php } else { ?>
-                                <p class="no-inventory-text">This product is currently unavailable</p>
-                            <?php } ?>
-
+                            </div>
                         </div>
                     </div>
+                <?php } ?>
+
+                <?php if (empty($this->products)) { ?>
+                    <p class="shop-only">No products available.</p>
+                <?php } ?>
+
+                <?php if (empty($this->shoppingCart)) { ?>
+                    <p class="cart-only">No products in your shopping cart.</p>
                 <?php } ?>
             </div>
         </div>
--- public/js/shop.js
+++ public/js/shop.js
@@ -1,17 +1,20 @@
-/**
- * Show or Hide an element by giving @id of the element
- * @param divID 
- */
 function toggleVisibility(divID) {
     const div = document.getElementById(divID);
-    div.style.display = (div.style.display === "none" || div.style.display === "") ? "flex" : "none";
+    div.style.display = (div.style.display === 'none' || div.style.display === '') ? 'flex' : 'none';
 }
 
-function switchVisibility(firstDiv, secondDiv, displayType = "block") {
-    const fDiv = document.getElementById(firstDiv);
-    const sDiv = document.getElementById(secondDiv);
+function toggleView() {
+    const container = document.getElementById('view-container');
+    const btn = document.getElementById('btn-toggle-view');
+    const isShop = container.className === 'mode-shop';
 
-    const firstIsVisible = window.getComputedStyle(fDiv).display !== "none";
-    fDiv.style.display = firstIsVisible ? "none" : displayType;
-    sDiv.style.display = firstIsVisible ? displayType : "none";
+    if (isShop) {
+        container.className = 'mode-cart';
+        btn.textContent = 'Back to Shop';
+        document.querySelectorAll('.product[data-in-cart="false"]').forEach(p => p.style.display = 'none');
+    } else {
+        container.className = 'mode-shop';
+        btn.textContent = 'View Shopping Cart';
+        document.querySelectorAll('.product').forEach(p => p.style.display = '');
+    }
 }
